c89dca72926ed71a36d967abf9f4395592d0228d
Artan Sinani <artisinani@gmail.com>
2017-01-30 23:46:57 +0000

    dummy home controller

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_current_user
+
+  protected
+
+  def set_current_user
+    session[:user_id] ||= Iconly::User.first.id
+  end
 end
---
+++ test/dummy/db/seeds.rb
@@ -0,0 +1,5 @@
+Iconly::User.create!(
+  name:  'John Smith',
+  email: 'john@example.com',
+  uid:   '1111AA'
+)
