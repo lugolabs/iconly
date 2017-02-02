fb5a80b2d60eed1ab7534d13ce30154c1525ec07
Artan Sinani <artisinani@gmail.com>
2017-02-02 01:57:45 +0000

    generate new project on user creation

--- test/models/iconly/user_test.rb
+++ test/models/iconly/user_test.rb
@@ -12,5 +12,14 @@ module Iconly
       assert_not user.valid?
       assert_equal ["Uid can't be blank", "Name can't be blank", "Email can't be blank"], user.errors.full_messages
     end
+
+    test 'should generate a project on create' do
+      user = User.create(uid: '11', name: 'Paul', email: 'paul@example.com')
+
+      assert_equal 1, user.projects.size
+      assert_equal 'Untitled Project', user.projects.first.name
+
+      user.destroy
+    end
   end
 end
